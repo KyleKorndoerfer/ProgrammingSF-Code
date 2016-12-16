ce884fe727f3ede0170545478ecb6b81da58300e
S. Kyle Korndoerfer <kyle.korndoerfer@gmail.com>
2016-12-16 11:56:44 -0500

    Updated code for Chapter 2 (WCF) added

---
+++ Chapter2/Wcf/CalculatorClient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+using CalculatorService.Interfaces;
+
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Client;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Client;
+
+namespace CalculatorClient
+{
+    internal class Program
+    {
+        static void Main( string[] args )
+        {
+            var iteration = 0;
+
+            while ( true )
+            {
+                var client = new ServiceClient( new Uri( "fabric:/CalculatorApplication/CalculatorService" ) );
+                var result = client.Add( 1, 2 ).GetAwaiter().GetResult();
+
+                Console.WriteLine( $"({++iteration:D4}) {result}" );
+
+                System.Threading.Thread.Sleep( TimeSpan.FromSeconds( 5 ) );
+            }
+        }
+    }
+
+    public class ServiceClient : ServicePartitionClient<WcfCommunicationClient<ICalculatorService>>, ICalculatorService
+    {
+        private static ICommunicationClientFactory<WcfCommunicationClient<ICalculatorService>> communicationClientFactory;
+
+        static ServiceClient()
+        {
+            communicationClientFactory = new WcfCommunicationClientFactory<ICalculatorService>( clientBinding: new BasicHttpBinding() );
+        }
+
+        public ServiceClient( Uri serviceUri ) : this( serviceUri, ServicePartitionKey.Singleton )
+        {
+        }
+
+        public ServiceClient( Uri serviceUri, ServicePartitionKey partitionKey ) : base( communicationClientFactory, serviceUri, partitionKey )
+        {
+        }
+
+        #region ICalculatorService
+
+        public Task<string> Add( int a, int b )
+        {
+            return this.InvokeWithRetry( c => c.Channel.Add( a, b ) );
+        }
+
+        public Task<string> Subtract( int a, int b )
+        {
+            return this.InvokeWithRetry( c => c.Channel.Subtract( a, b ) );
+        }
+
+        #endregion
+    }
+}
---
+++ Chapter2/Wcf/CalculatorService/CalculatorService.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.Fabric;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+using CalculatorService.Interfaces;
+
+using Microsoft.ServiceFabric.Services.Communication.Runtime;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Runtime;
+using Microsoft.ServiceFabric.Services.Runtime;
+
+
+namespace CalculatorService
+{
+	/// <summary>
+	/// An instance of this class is created for each service instance by the Service Fabric runtime.
+	/// </summary>
+	internal sealed class CalculatorService : StatelessService, ICalculatorService
+	{
+		public CalculatorService( StatelessServiceContext context ) : base( context )
+		{
+		}
+
+		#region StatelessService overrides
+
+		protected override IEnumerable<ServiceInstanceListener> CreateServiceInstanceListeners()
+		{
+            return new[]
+            {
+                new ServiceInstanceListener( context => new WcfCommunicationListener<ICalculatorService>(
+                    serviceContext: context,
+                    wcfServiceObject: this,
+                    listenerBinding: new BasicHttpBinding(),
+                    endpointResourceName: "ServiceEndpoint" ) )
+			};
+		}
+
+		#endregion
+
+		#region ICalculatorService implementation
+
+		public Task<string> Add( int a, int b )
+		{
+			var result = a + b;
+
+			ServiceEventSource.Current.ServiceMessage( Context, "Received Add({0}, {1})", a, b);
+			ServiceEventSource.Current.ServiceMessage( Context, "Return {0}", result );
+
+			return Task.FromResult<string>( $"Instance {Context.InstanceId} returns: {result}" );
+		}
+
+		public Task<string> Subtract( int a, int b )
+		{
+			var result = a - b;
+
+			ServiceEventSource.Current.ServiceMessage( Context, "Received Subtract({0}, {1})", a, b );
+			ServiceEventSource.Current.ServiceMessage( Context, "Return {0}", result );
+
+            return Task.FromResult<string>( $"Instance {Context.InstanceId} returns: {result}" );
+        }
+
+		#endregion
+	}
+}
