9b2df7515e0704f33ad25120cf91c135d017188b
S. Kyle Korndoerfer <kyle.korndoerfer@xerox.com>
2017-01-04 17:18:54 -0500

    Chapter 3 - SimpleStoreApplication

---
+++ Chapter3/SimpleStoreApplication/SimpleStoreClient/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Common;
+
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Client;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Client;
+
+namespace SimpleStoreClient
+{
+	public class Client : ServicePartitionClient<WcfCommunicationClient<IShoppingCartService>>, IShoppingCartService
+	{
+		public Client( WcfCommunicationClientFactory<IShoppingCartService> clientFactory, Uri serviceUri )
+			: base( clientFactory, serviceUri, new ServicePartitionKey(1) )
+		{
+		}
+
+		#region Implementation of IShoppingCartService
+
+		public Task AddItem( ShoppingCartItem item )
+		{
+			return InvokeWithRetryAsync( client => client.Channel.AddItem( item ) );
+		}
+
+		public Task DeleteItem( string productName )
+		{
+			return InvokeWithRetryAsync( client => client.Channel.DeleteItem( productName ) );
+		}
+
+		public Task<List<ShoppingCartItem>> GetItems()
+		{
+			return InvokeWithRetryAsync( client => client.Channel.GetItems() );
+		}
+
+		#endregion
+	}
+}
---
+++ Chapter3/SimpleStoreApplication/SimpleStoreClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Fabric;
+using System.ServiceModel.Channels;
+
+using Common;
+
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Wcf;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Client;
+
+namespace SimpleStoreClient
+{
+	class Program
+	{
+		static void Main( string[] args )
+		{
+			Uri serviceUri = new Uri( "fabric:/SimpleStoreApplication/ShoppingCartService" );
+
+			Binding binding = WcfUtility.CreateTcpClientBinding();
+			IServicePartitionResolver partitionResolver = new ServicePartitionResolver( () => new FabricClient() );
+
+			Client shoppingClient = new Client(
+					new WcfCommunicationClientFactory<IShoppingCartService>( binding, null, partitionResolver ),
+					serviceUri );
+
+			shoppingClient.AddItem( new ShoppingCartItem
+			{
+				ProductName = "XBOX ONE",
+				UnitPrice = 329.0,
+				Amount = 2
+			} ).Wait();
+
+			var list = shoppingClient.GetItems().Result;
+			foreach (var item in list)
+			{
+				Console.WriteLine( $"{item.ProductName}: {item.UnitPrice:C2} x {item.Amount} = {item.LineTotal:C2}" );
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
