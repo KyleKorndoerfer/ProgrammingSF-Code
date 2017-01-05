6262a0381dd112b1b39eb351656e6b8f0c6e98b1
S. Kyle Korndoerfer <kyle.korndoerfer@xerox.com>
2017-01-05 12:37:09 -0500

    Chapter 3 - SimpleStoreApplication (Named Partitions)

---
+++ Chapter3/SimpleStoreApplication-NamedPartitions/SimpleStoreClient/Client.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Common;
+
+using Microsoft.ServiceFabric.Services.Client;
+using Microsoft.ServiceFabric.Services.Communication.Client;
+using Microsoft.ServiceFabric.Services.Communication.Wcf.Client;
+
+namespace SimpleStoreClient
+{
+	public class Client : ServicePartitionClient<WcfCommunicationClient<IShoppingCartService>>, IShoppingCartService
+	{
+		public Client( WcfCommunicationClientFactory<IShoppingCartService> clientFactory, Uri serviceUri, string customerId )
+			: base( clientFactory, serviceUri, new ServicePartitionKey(customerId) )
+		{
+		}
+
+		#region Implementation of IShoppingCartService
+
+		public Task AddItem( ShoppingCartItem item )
+		{
+			return InvokeWithRetryAsync( client => client.Channel.AddItem( item ) );
+		}
+
+		public Task DeleteItem( string productName )
+		{
+			return InvokeWithRetryAsync( client => client.Channel.DeleteItem( productName ) );
+		}
+
+		public Task<List<ShoppingCartItem>> GetItems()
+		{
+			return InvokeWithRetryAsync( client => client.Channel.GetItems() );
+		}
+
+		#endregion
+	}
+}
